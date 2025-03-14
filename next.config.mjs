02c7a8ea88d92e42a177b0126c1f48bb23e57b4a
chuanting.xu <ctrl.xu@foxmail.com>
2025-03-14 11:24:25 +0800

    config export

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            // https://picsum.photos/200/200
-            protocol: 'https',
-            hostname: '*',
-            port: '',
-          },
-        ],
+  output: "export",
+  images: {
+    remotePatterns: [
+      {
+        // https://picsum.photos/200/200
+        protocol: "https",
+        hostname: "*",
+        port: "",
       },
+    ],
+  },
 };
 
 export default nextConfig;
